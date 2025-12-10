e5857ff344960e7d7a8fb69aac541969d1fcecad
ZaZza402 <ytzazza@gmail.com>
2025-12-10 14:22:44 +0100

    Fix robots.txt to use static sitemap, prioritize logo for search results

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/admin/"],
     },
-    sitemap: "https://bur0.click/sitemap.xml",
+    sitemap: "https://bur0.click/sitemap-static.xml",
   };
 }
