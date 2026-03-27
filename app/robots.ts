eb16b04421d060fc6a38c92c99d928120a821a8d
ZaZza402 <ytzazza@gmail.com>
2026-03-27 14:16:36 +0100

    added www. to domain for sitemap robots

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/admin/", "/privacy", "/termini", "/upgrade"],
     },
-    sitemap: "https://bur0.click/sitemap.xml",
+    sitemap: "https://www.bur0.click/sitemap.xml",
   };
 }
